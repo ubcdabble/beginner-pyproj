ed6112a623e9e79fe46d224aba0277fb508909c1
Dabble <seungkwan.kim@tx-inc.com>
2025-04-24 19:58:39 +0900

    Set up processRequest()

--- tasktracker.py
+++ tasktracker.py
@@ -1,34 +1,71 @@
 import os
 import json
+import argparse
 
 filename = 'tasks.json'
 foldername = '~/Documents/Projects/beginner-pyproj/tasktracker'
 
 folderpath = os.path.expanduser(foldername)
-print(folderpath)
-
 filepath = os.path.join(folderpath, filename)
-os.makedirs(folderpath, exist_ok=True)
 
-if os.path.exists(filepath):
-    with open(filepath, 'r') as taskfile:
-        print(taskfile.read())
-
-else:   
-    with open(filepath, 'w') as taskfile:
-        username = input('What is your name: ')
-        taskfile.write(f"{username}'s Task List")
-
-    print(f'File created at {filepath}')
-
-# def taskSelect():
-#     print()
+def createFile():
     
-# def checkJSON():
-#     if os.path.exists(filename):
-#         print('Welcome back!')
-#     else:
-#         print('Hello new user')
+    os.makedirs(folderpath, exist_ok=True)
 
-# test = input("Please enter something")
-# print(f'You entered: {test}')
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as taskfile:
+            y = json.load(taskfile)
+        print(y)
+            
+    else:   
+        with open(filepath, 'w') as taskfile:
+            json.dump([], taskfile, indent=4) 
+        print(f'File created at {filepath}')
+
+def processRequest():
+    parser = argparse.ArgumentParser(description="Create or edit a task list")
+    sub_parser = parser.add_subparsers(dest='command', description="Command")
+    
+    add_task = sub_parser.add_parser('add')
+    add_task.add_argument('task', help="Task name")
+    
+    update_task = sub_parser.add_parser('update')
+    update_task.add_argument('id', help="ID number")
+    update_task.add_argument('task', help="Task name")
+    
+    delete_task = sub_parser.add_parser('delete')
+    delete_task.add_argument('identifier', help="Identifier (ID number or task name)")
+    
+    mark_in_progress = sub_parser.add_parser('mark-in-progress')
+    mark_in_progress.add_argument('identifier', help="Identifier (ID number or task name)")
+    
+    mark_done = sub_parser.add_parser('mark-done')
+    mark_done.add_argument('identifier', help="Identifier (ID number or task name)")
+    
+    list_tasks = sub_parser.add_parser('list')
+    list_tasks.add_argument('status', help="List type to view: done, todo, in-progress")
+    
+    args = parser.parse_args()
+    
+    print(f'You want to {args.command}')
+    
+    match args.command:
+        case 'add':
+            print('add')
+        case 'update':
+            print('update')
+        case 'delete' | 'mark-in-progress' | 'mark-done':
+            print('delete, mark in progress, mark done')
+        case 'list':
+            print('list')
+
+def addTask(taskname, status):
+    with open(filepath, 'w') as taskfile:
+        json.dump('[]', taskfile, indent=4)
+    
+
+
+
+if __name__ == '__main__':
+    createFile()
+    processRequest()
